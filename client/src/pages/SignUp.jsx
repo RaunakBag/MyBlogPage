8f2033c201abe5ae31796a29ddd854cd1fb9bfc1
Raunak Bag <raunakbag97@gmail.com>
2024-02-18 20:22:09 +0530

    Completed SignIn functionality and UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -76,7 +76,7 @@ export default function SignUp() {
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to='/sign-in' className="text-blue-500">SignIn</Link>
+            <Link to='/sign-in' className="text-blue-500">Sign In</Link>
           </div>
           {errorMessage && (
             <Alert className="mt-5" color='failure'>
