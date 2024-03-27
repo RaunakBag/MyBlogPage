f72570ee5b1319a5482560da465213da77eb1e92
Raunak Bag <raunakbag97@gmail.com>
2024-03-27 11:30:44 +0530

    Revert "Minor Changes"
    This reverts commit 5497913ef456784d8459df5b85b9640edca55773.

--- api/index.js
+++ api/index.js
@@ -23,30 +23,30 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
+    console.log('Server is running on port 3000');
+})
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/post', postRoutes);
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
-});
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message ||'Internal Server Error';
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+})
